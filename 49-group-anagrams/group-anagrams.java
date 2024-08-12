a7ed66b2ceae8baaa8ada3da6dc0e9059babb961
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-08-13 00:03:18 +0530

    Time: 11 ms (26.25%) | Memory: 48.5 MB (12.47%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,34 @@
+import java.math.BigInteger;
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return null;
+        
+        int nos[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103};
+        
+        Map<BigInteger, List<String>> map = new HashMap<>();
+        
+        for (String word : strs) {
+            BigInteger prod = BigInteger.ONE;
+            for (int i = 0; i < word.length(); i++) {
+                prod = prod.multiply(BigInteger.valueOf(nos[word.charAt(i) - 'a']));
+            }
+            
+            if (map.containsKey(prod)) {
+                map.get(prod).add(word);
+            } else {
+                map.put(prod, new ArrayList<String>());
+                map.get(prod).add(word);
+            }
+        }
+        
+        List<List<String>> result = new ArrayList<>();
+        for (List<String> val : map.values()) {
+            result.add(val);
+        }
+        
+        return result;
+    }
+}
