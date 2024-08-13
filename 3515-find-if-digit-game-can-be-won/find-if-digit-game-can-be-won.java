d2dbe00208589217fe561b40af4cfda40317ef1f
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-08-13 23:06:43 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (42.84%) - LeetSync

--- 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.java
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.java
@@ -3,7 +3,7 @@ class Solution {
         int s1=0,s2=0;
         for(int i:nums)
         {
-            if(i>=0 && i<=9)
+            if(i<10)
                 s1+=i;
             else
                 s2+=i;
