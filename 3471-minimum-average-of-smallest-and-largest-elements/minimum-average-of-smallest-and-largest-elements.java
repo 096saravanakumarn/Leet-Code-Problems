80da63a9766411f34c4785dd9ec9a56466e9d218
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-08-13 23:44:22 +0530

    Time: 2 ms (98.24%) | Memory: 43.8 MB (36.97%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.java
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.java
@@ -4,19 +4,13 @@ class Solution {
         Arrays.sort(arr);
         int i = 0;
         int j = arr.length-1;
-        //List<Double> list = new ArrayList<>();
         double min = Double.MAX_VALUE;
         while(i<j){
             double avg = (arr[i]+arr[j])/2d;
-            if(min > avg){
-                min = avg;
-            }
-            //list.add(avg);
+            min = Math.min(min,avg);
             i++;
             j--;
         }
-       // Collections.sort(list);
-       // return list.get(0);
        return min;
     }
 }
