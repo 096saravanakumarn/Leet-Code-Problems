4a26d656ecb92e7725fc77b8e9ed5d45895592f6
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-08-19 23:59:37 +0530

    Time: 19 ms (6.01%) | Memory: 42.7 MB (6.12%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -4,6 +4,7 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         for(int i:arr){
             map.put(i,map.getOrDefault(i, 0) + 1);
+            System.out.println(map.getOrDefault(i, 0) + 1);
         }
         Set<Integer> set = new HashSet<>(map.values());
         return map.size() == set.size();
