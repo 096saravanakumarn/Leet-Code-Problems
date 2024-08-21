21a5dc4f083d631c52ffd351b78bbec8ec6573bf
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-08-22 01:01:26 +0530

    Time: 1 ms (48.67%) | Memory: 44.8 MB (14.74%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.java
+++ 861-flipping-an-image/flipping-an-image.java
@@ -3,19 +3,18 @@ class Solution {
         int r = image.length;
         int c = image.length;
         int arr[][] = new int[r][c];
-        for(int i=0;i<r;i++){
+        for (int i = 0; i < r; i++) {
             int k = 0;
-            for(int j=c-1;j>=0;j--){
-                arr[i][k] = image[i][j]; 
+            for (int j = c - 1; j >= 0; j--) {
+                arr[i][k] = image[i][j];
                 k++;
             }
         }
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(arr[i][j] == 1){
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (arr[i][j] == 1) {
                     arr[i][j] = 0;
-                }
-                else{
+                } else {
                     arr[i][j] = 1;
                 }
             }
