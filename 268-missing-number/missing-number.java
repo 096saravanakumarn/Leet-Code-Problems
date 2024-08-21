e87dc02e102110ac504da318fa199faeea6a07bf
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-08-22 00:19:53 +0530

    Time: 5 ms (25.64%) | Memory: 45.3 MB (39.44%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,27 +1,13 @@
-// import java.util.*;
-// class Solution {
-//     public int missingNumber(int[] nums) {
-//         List<Integer> l = new ArrayList<>();
-//         for(int i:nums){
-//             l.add(i);
-//         }
-//         for(int i=0;i<nums.length+1;i++){
-//             if(!l.contains(i)){
-//                 return i;
-//             }
-//         }
-//         return -1;
-//     }
-// }
+import java.util.*;
 class Solution {
     public int missingNumber(int[] nums) {
-        List<Integer> set = new ArrayList<>();
-        for(int n : nums){
-           set.add(n);
+        HashSet<Integer> l = new HashSet<>();
+        for(int i:nums){
+            l.add(i);
         }
-        for(int i=0; i<=nums.length; i++){
-            if(!set.contains(i)){
-               return i;
+        for(int i=0;i<nums.length+1;i++){
+            if(!l.contains(i)){
+                return i;
             }
         }
         return -1;
