875da4256b3cde3ee84701c6576933e975d13151
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-08-21 21:46:47 +0530

    Time: 5 ms (25.27%) | Memory: 42.5 MB (14.97%) - LeetSync

--- 1944-truncate-sentence/truncate-sentence.java
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -1,12 +1,12 @@
 class Solution {
     public String truncateSentence(String s, int k) {
         String st[] = s.split(" ");
-        String s1="";
-        for(int i=0;i<k;i++){
-            if(i!=k-1)
-                s1+=st[i]+" ";
+        String s1 = "";
+        for (int i = 0; i < k; i++) {
+            if (i != k - 1)
+                s1 += st[i] + " ";
             else
-                s1+=st[i];
+                s1 += st[i];
         }
         return s1;
     }
