7a04fc956bde7c9514cbf7d871da81a52ef4d56d
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-08-21 22:21:48 +0530

    Time: 13 ms (5.82%) | Memory: 44.2 MB (45.68%) - LeetSync

--- 2698-find-the-array-concatenation-value/find-the-array-concatenation-value.java
+++ 2698-find-the-array-concatenation-value/find-the-array-concatenation-value.java
@@ -1,17 +1,17 @@
 class Solution {
     public long findTheArrayConcVal(int[] nums) {
         int l = 0;
-        int r = nums.length-1;
-        long sum=0;
-        while(l<r){
-                String s = "";
-                s+=nums[l]+""+nums[r];
-                sum+=Integer.parseInt(s);
-                l++;
-                r--;
+        int r = nums.length - 1;
+        long sum = 0;
+        while (l < r) {
+            String s = "";
+            s += nums[l] + "" + nums[r];
+            sum += Integer.parseInt(s);
+            l++;
+            r--;
         }
-        if(nums.length%2 != 0){
-                sum+=nums[nums.length/2];
+        if (nums.length % 2 != 0) {
+            sum += nums[nums.length / 2];
         }
         return sum;
     }
