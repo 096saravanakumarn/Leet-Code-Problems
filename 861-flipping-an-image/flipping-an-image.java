86a02f429327ec082d19f3aa82463e3e33a68bda
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-08-22 01:00:15 +0530

    Time: 1 ms (48.67%) | Memory: 44.5 MB (40.27%) - LeetSync

---
+++ 861-flipping-an-image/flipping-an-image.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int r = image.length;
+        int c = image.length;
+        int arr[][] = new int[r][c];
+        for(int i=0;i<r;i++){
+            int k = 0;
+            for(int j=c-1;j>=0;j--){
+                arr[i][k] = image[i][j]; 
+                k++;
+            }
+        }
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(arr[i][j] == 1){
+                    arr[i][j] = 0;
+                }
+                else{
+                    arr[i][j] = 1;
+                }
+            }
+        }
+        return arr;
+    }
+}
