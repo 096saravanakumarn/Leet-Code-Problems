a0a067928eea162514c7d91f7e76a178156b806e
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-08-23 00:19:45 +0530

    Time: 1 ms (49.58%) | Memory: 44 MB (14.42%) - LeetSync

--- 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.java
+++ 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.java
@@ -3,9 +3,8 @@ class Solution {
         int c = 0;
         for(int i=0;i<words.length;i++){
             String s1 = "";
-            int l = words[i].length();
             if(words[i].length() <= s.length())
-                s1 = s.substring(0,l);
+                s1 = s.substring(0,words[i].length());
             if(words[i].equals(s1)){
                 c++;
             }
