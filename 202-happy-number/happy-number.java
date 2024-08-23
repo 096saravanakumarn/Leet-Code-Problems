143ffe142112abe2e53f1a3deeaf8177240ee0d3
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-08-24 00:19:25 +0530

    Time: 16 ms (5.14%) | Memory: 41 MB (13.76%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int sum = 0;
+        while(n>5){
+            sum = 0;
+            while(n>0){
+                int a = n%10;
+                sum+=a*a;
+                n/=10;
+                 System.out.print(sum);
+            }
+            n = sum;
+             
+        }
+       
+        return n==1;
+    }
+}
