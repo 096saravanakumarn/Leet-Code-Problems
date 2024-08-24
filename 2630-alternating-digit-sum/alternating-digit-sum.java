7fa416804acc3fb712e81917ecc9ac3b477b8ecc
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-08-24 18:49:22 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (22.27%) - LeetSync

--- 2630-alternating-digit-sum/alternating-digit-sum.java
+++ 2630-alternating-digit-sum/alternating-digit-sum.java
@@ -1,18 +1,13 @@
 class Solution {
     public int alternateDigitSum(int n) {
         String s = Integer.toString(n);
-        int arr[] = new int[s.length()]; 
-        for(int i=0;i<arr.length;i++){
-            arr[i] = Integer.parseInt(s.charAt(i)+"");
-        }
         int sum = 0;
-        for(int i=0;i<arr.length;i++){
-            if(i%2 == 0){
-                sum+=arr[i];
-            }
-            else{
-                sum+=arr[i]*-1;
-            }
+        for(int i=0;i<s.length();i++){
+            int d = Character.getNumericValue(s.charAt(i));
+            if(i%2 == 0)
+                sum+=d;
+            else
+                sum+=d*-1;
         }
         return sum;
     }
