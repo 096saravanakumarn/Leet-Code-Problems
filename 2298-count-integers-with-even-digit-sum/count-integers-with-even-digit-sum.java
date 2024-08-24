8d70ac182988a00d0ee1edfb2735045c187d85ab
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-08-24 18:23:40 +0530

    Time: 1 ms (93.71%) | Memory: 40.3 MB (44.51%) - LeetSync

--- 2298-count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.java
+++ 2298-count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.java
@@ -7,7 +7,7 @@ class Solution {
             sum = 0;
             while(n>0){
                 sum+=n%10;
-                n=n/10;
+                n/=10;
             }
             if(sum%2==0)
                 c++;
