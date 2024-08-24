074707ec68691a6ca91782455f168c731471af71
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-08-24 18:06:57 +0530

    Time: 10 ms (9.51%) | Memory: 45.1 MB (5.85%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.java
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.java
@@ -1,16 +1,17 @@
 import java.util.*;
 import java.math.BigInteger;
+
 class Solution {
     public int getLucky(String s, int k) {
         String st = "";
-        for(int i=0;i<s.length();i++){
-             st += Integer.toString((s.charAt(i)-'a')+1);
+        for (int i = 0; i < s.length(); i++) {
+            st += Integer.toString((s.charAt(i) - 'a') + 1);
         }
         BigInteger num = new BigInteger(st);
-        BigInteger sum ;
-       for(int i=0;i<k;i++){
-        sum  = BigInteger.ZERO;
-            while(num.compareTo(BigInteger.ZERO) != 0){
+        BigInteger sum;
+        for (int i = 0; i < k; i++) {
+            sum = BigInteger.ZERO;
+            while (num.compareTo(BigInteger.ZERO) > 0) {
                 BigInteger a = num.mod(BigInteger.TEN);
                 sum = sum.add(a);
                 num = num.divide(BigInteger.TEN);
@@ -18,6 +19,6 @@ class Solution {
             num = sum;
         }
         int l = num.intValue();
-        return l; 
+        return l;
     }
 }
