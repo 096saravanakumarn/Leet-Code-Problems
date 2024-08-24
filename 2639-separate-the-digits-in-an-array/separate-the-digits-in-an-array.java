8457e5cc4340a242ac2c122b48ef25df666cc748
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-08-24 18:39:28 +0530

    Time: 10 ms (13.60%) | Memory: 45.5 MB (11.76%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
@@ -5,13 +5,11 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             String s = "";
             while(nums[i]>0){
-                int a = nums[i]%10;
-                s+=Integer.toString(a);
+                s+=Integer.toString(nums[i]%10);
                 nums[i]/=10;
             }
-           
-                for(int j=s.length()-1;j>=0;j--)
-                    list.add(Integer.parseInt(s.charAt(j)+""));
+            for(int j=s.length()-1;j>=0;j--)
+                list.add(Integer.parseInt(s.charAt(j)+""));
         }
         int arr[] = new int[list.size()];
         for(int i=0;i<list.size();i++){
