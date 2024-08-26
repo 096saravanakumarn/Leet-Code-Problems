b59a2618cc771055736e303a8ef6e62d6c5b6d32
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-08-26 19:46:23 +0530

    Time: 41 ms (10.74%) | Memory: 44.4 MB (19.90%) - LeetSync

--- 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.java
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.java
@@ -6,7 +6,6 @@ class Solution {
         for (int i = 0; i < arr.length; i++) {
             map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
         }
-        System.out.print(map);
         for (String ch : map.keySet()) {
             System.out.print(ch);
             if (map.get(ch) == 1) {
