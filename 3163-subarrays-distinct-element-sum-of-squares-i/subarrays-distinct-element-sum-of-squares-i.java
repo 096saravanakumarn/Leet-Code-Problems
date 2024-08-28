fbfb88b8edab6af612f4e7362207dbcfe98d41f1
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-08-28 13:06:46 +0530

    Time: 12 ms (45.51%) | Memory: 44.8 MB (45.82%) - LeetSync

--- 3163-subarrays-distinct-element-sum-of-squares-i/subarrays-distinct-element-sum-of-squares-i.java
+++ 3163-subarrays-distinct-element-sum-of-squares-i/subarrays-distinct-element-sum-of-squares-i.java
@@ -5,11 +5,9 @@ class Solution {
         for(int i=0;i<nums.size();i++){
             Set<Integer> set = new HashSet<>();
             for(int j=i;j<nums.size();j++){
-                System.out.print(nums.get(j)+" ");
                 set.add(nums.get(j));
                 result += set.size()*set.size();
             }
-            System.out.println();
         }
         return result;
     }
