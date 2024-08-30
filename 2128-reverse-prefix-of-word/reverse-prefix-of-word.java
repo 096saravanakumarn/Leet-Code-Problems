1b630360a294e1ac381eb145ef0601a4a9b972e6
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-08-30 15:52:08 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (98.23%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
@@ -1,23 +1,23 @@
 import java.util.*;
+
 class Solution {
     public String reversePrefix(String word, char ch) {
         StringBuilder st = new StringBuilder();
         int ind = 0;
-        for(int i=0;i<word.length();i++){
-            if(word.charAt(i) != ch){
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != ch) {
                 st.append(word.charAt(i));
-                if(i == word.length()-1){
+                if (i == word.length() - 1) {
                     return st.toString();
                 }
-            }
-            else{
+            } else {
                 st.append(word.charAt(i));
                 st.reverse();
-                ind = i+1;
+                ind = i + 1;
                 break;
             }
         }
-        st.append(word.substring(ind,word.length()));
+        st.append(word.substring(ind, word.length()));
         return st.toString();
     }
 }
