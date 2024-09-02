b15ca91d0d73f40e8efca20973c6886731aeb6e3
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-02 19:11:59 +0530

    Time: 2322 ms (5.04%) | Memory: 59.9 MB (52.24%) - LeetSync

---
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int chalkReplacer(int[] arr, int k) {
+        int i = 0;
+        while(k>=arr[i]){
+                k = k - arr[i];
+            if(i!=arr.length-1){
+                i++;
+            }
+            else{
+                i = 0;
+            }
+        }
+        return i;
+    }
+}
