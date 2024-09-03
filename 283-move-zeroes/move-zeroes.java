91559dc06cc7376bf2e811de4403f5a105d21f01
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-03 22:59:23 +0530

    Time: 2 ms (82.90%) | Memory: 46.1 MB (21.34%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,24 +1,36 @@
 import java.util.*;
 class Solution {
     public void moveZeroes(int[] nums) {
-        List<Integer> l1 = new ArrayList<>();
-        List<Integer> l2 = new ArrayList<>();
+        // List<Integer> l1 = new ArrayList<>();
+        // List<Integer> l2 = new ArrayList<>();
+        // int c = 0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i] != 0){
+        //       l1.add(nums[i]);
+        //     }    
+        //     else{
+        //        l2.add(0);
+        //     } 
+        // }
+        //     for(int i=0;i<l1.size();i++){
+        //         nums[i] = l1.get(i);
+        //     }
+        //     int k = 0;
+        //     for(int i=l1.size();i<nums.length;i++){
+        //         nums[i] = l2.get(k++);
+        //     }
         int c = 0;
+        int k = 0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i] != 0){
-              l1.add(nums[i]);
-            }    
-            else{
-               l2.add(0);
-            } 
+            if(nums[i]!=0){
+                nums[k++] = nums[i];
+                c++; 
+            }
         }
-            for(int i=0;i<l1.size();i++){
-                nums[i] = l1.get(i);
+        //if(c<nums.length){
+            for(int i=k;i<nums.length;i++){
+                nums[k++] = 0;
             }
-            int k = 0;
-            for(int i=l1.size();i<nums.length;i++){
-                nums[i] = l2.get(k++);
-            }
-        
+       // }
     }
 }
