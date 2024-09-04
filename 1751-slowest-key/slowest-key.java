a954de103aff6bb6483b2d940c6d3d757eb82814
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-04 23:36:29 +0530

    Time: 2 ms (18.81%) | Memory: 44.5 MB (27.75%) - LeetSync

--- 1751-slowest-key/slowest-key.java
+++ 1751-slowest-key/slowest-key.java
@@ -1,24 +1,25 @@
 import java.util.*;
+
 class Solution {
     public char slowestKey(int[] releaseTimes, String keysPressed) {
-        int arr[] = new int [releaseTimes.length];
+        int arr[] = new int[releaseTimes.length];
         arr[0] = releaseTimes[0];
-        for(int i=1;i<releaseTimes.length;i++){
-           int t = releaseTimes[i]-releaseTimes[i-1];
-           arr[i] = t;
+        for (int i = 1; i < releaseTimes.length; i++) {
+            int t = releaseTimes[i] - releaseTimes[i - 1];
+            arr[i] = t;
         }
         int max = 0;
-        for(int i=0;i<arr.length;i++){
-            if(max<=arr[i]){
+        for (int i = 0; i < arr.length; i++) {
+            if (max <= arr[i]) {
                 max = arr[i];
             }
-        } 
-        List<Character> list = new ArrayList<>(); 
-        for(int i=0;i<arr.length;i++){
-            if(arr[i] == max)
+        }
+        List<Character> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == max)
                 list.add(keysPressed.charAt(i));
         }
         Collections.sort(list);
-        return list.get(list.size()-1);
+        return list.get(list.size() - 1);
     }
 }
