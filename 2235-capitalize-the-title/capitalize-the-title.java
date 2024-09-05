b3df0816bf24893d028b12c802853e22f1fadb34
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-05 22:58:22 +0530

    Time: 4 ms (54.27%) | Memory: 42.8 MB (37.90%) - LeetSync

--- 2235-capitalize-the-title/capitalize-the-title.java
+++ 2235-capitalize-the-title/capitalize-the-title.java
@@ -2,28 +2,26 @@ class Solution {
     public String capitalizeTitle(String title) {
         String st[] = title.split(" ");
         StringBuilder str = new StringBuilder();
-        for(int i=0;i<st.length;i++){
-            for(int j=0;j<st[i].length();j++){
-                if(Character.isUpperCase(st[i].charAt(j))){
+        for (int i = 0; i < st.length; i++) {
+            for (int j = 0; j < st[i].length(); j++) {
+                if (Character.isUpperCase(st[i].charAt(j))) {
                     str.append(Character.toLowerCase(st[i].charAt(j)));
-                }
-                else
+                } else
                     str.append(st[i].charAt(j));
             }
             str.append(" ");
         }
         String st1[] = str.toString().split(" ");
         StringBuilder s1 = new StringBuilder();
-        for(int i=0;i<st1.length;i++){
-            for(int j=0;j<st1[i].length();j++){
-                if(j==0 && st1[i].length()>2){
+        for (int i = 0; i < st1.length; i++) {
+            for (int j = 0; j < st1[i].length(); j++) {
+                if (j == 0 && st1[i].length() > 2) {
                     s1.append(Character.toUpperCase(st1[i].charAt(j)));
-                }
-                else
+                } else
                     s1.append(st1[i].charAt(j));
             }
-            if(i!=st1.length-1)
-            s1.append(" ");
+            if (i != st1.length - 1)
+                s1.append(" ");
         }
         return s1.toString();
     }
