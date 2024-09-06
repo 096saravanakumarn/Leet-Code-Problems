ffd64ca926fbc48495a9e149c6944e615bf0193d
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-06 22:53:20 +0530

    Time: 4 ms (72.15%) | Memory: 44.6 MB (39.36%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -6,12 +6,12 @@ class Solution {
         char ch1[] = t.toCharArray();
         Arrays.sort(ch);
         Arrays.sort(ch1);
-        String s1 = "";
-        String s2 = "";
-        for(int i=0;i<ch.length;i++){
-            s1+=ch[i];
-            s2+=ch1[i];
-        }
-        return s1.equals(s2);
+        // String s1 = "";
+        // String s2 = "";
+        // for(int i=0;i<ch.length;i++){
+        //     s1+=ch[i];
+        //     s2+=ch1[i];
+        // }
+        return Arrays.equals(ch, ch1);
     }
 }
