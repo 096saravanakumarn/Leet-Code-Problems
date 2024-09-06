d3bac55c38c9a5e4f21fd6b2a8551ef7cb03830d
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-06 18:02:06 +0530

    Time: 1 ms (24.11%) | Memory: 41.7 MB (21.09%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,8 +1,8 @@
 class Solution {
     public String toLowerCase(String s) {
         StringBuilder st = new StringBuilder();
-        for(char c : s.toCharArray()){
-            if(Character.isUpperCase(c))
+        for (char c : s.toCharArray()) {
+            if (Character.isUpperCase(c))
                 st.append(Character.toLowerCase(c));
             else
                 st.append(c);
