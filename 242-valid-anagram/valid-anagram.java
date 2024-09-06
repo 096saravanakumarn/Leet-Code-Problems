1ff20c3afa8c056983ec840f31239f8d6b0d1620
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-06 22:49:24 +0530

    Time: 243 ms (5.31%) | Memory: 46.5 MB (5.53%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+            return false;
+        char ch[] = s.toCharArray();
+        char ch1[] = t.toCharArray();
+        Arrays.sort(ch);
+        Arrays.sort(ch1);
+        String s1 = "";
+        String s2 = "";
+        for(int i=0;i<ch.length;i++){
+            s1+=ch[i];
+            s2+=ch1[i];
+        }
+        return s1.equals(s2);
+    }
+}
