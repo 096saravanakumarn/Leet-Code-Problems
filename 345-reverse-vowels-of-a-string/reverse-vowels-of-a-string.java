7cc8671e05d650a5500a0616b9fb5ce2ac25560a
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-08 15:47:14 +0530

    Time: 7 ms (26.34%) | Memory: 45.3 MB (23.88%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -7,11 +7,11 @@ class Solution {
             if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u'||
             s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U'){
                 stack.push(s.charAt(i));
-                ch[i] = '/';
+                ch[i] = '*';
             }
         }
         for(int i=0;i<s.length();i++){
-            if(ch[i] == '/' && !stack.isEmpty())
+            if(ch[i] == '*' && !stack.isEmpty())
                 ch[i] = stack.pop();
                 sb.append(ch[i]);
         }
