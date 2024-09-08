a52703d1b3ae6767c5ba919d2e8e95f02026fd4d
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-08 15:49:50 +0530

    Time: 7 ms (26.34%) | Memory: 45 MB (37.43%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -5,7 +5,7 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<s.length();i++){
             if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u'||
-            s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U'){
+                s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U'){
                 stack.push(s.charAt(i));
                 ch[i] = '*';
             }
