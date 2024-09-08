ce5e40d2e75c1c06e45bb886976dec76792eb6f7
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-08 15:50:56 +0530

    Time: 7 ms (26.34%) | Memory: 45.2 MB (23.88%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -3,17 +3,19 @@ class Solution {
         Stack<Character> stack = new Stack<>();
         char ch[] = s.toCharArray();
         StringBuilder sb = new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u'||
-                s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U'){
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o'
+                    || s.charAt(i) == 'u' ||
+                    s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O'
+                    || s.charAt(i) == 'U') {
                 stack.push(s.charAt(i));
                 ch[i] = '*';
             }
         }
-        for(int i=0;i<s.length();i++){
-            if(ch[i] == '*' && !stack.isEmpty())
+        for (int i = 0; i < s.length(); i++) {
+            if (ch[i] == '*' && !stack.isEmpty())
                 ch[i] = stack.pop();
-                sb.append(ch[i]);
+            sb.append(ch[i]);
         }
         return sb.toString();
     }
