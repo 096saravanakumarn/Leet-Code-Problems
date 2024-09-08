d1c6f289093deddbcb666441337e04cd6ac66e9f
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-08 15:42:48 +0530

    Time: 7 ms (26.34%) | Memory: 45.2 MB (28.91%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -2,6 +2,7 @@ class Solution {
     public String reverseVowels(String s) {
         Stack<Character> stack = new Stack<>();
         char ch[] = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
         for(int i=0;i<s.length();i++){
             if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u'||
             s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U'){
@@ -9,7 +10,7 @@ class Solution {
                 ch[i] = '/';
             }
         }
-        StringBuilder sb = new StringBuilder();
+       
         for(int i=0;i<s.length();i++){
             if(ch[i] == '/' && !stack.isEmpty())
                 ch[i] = stack.pop();
