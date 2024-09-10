9104312f139f97bdc8574bb76a323083f6df79cc
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-11 00:27:56 +0530

    Time: 10 ms (36.43%) | Memory: 59.4 MB (22.79%) - LeetSync

--- 2722-prime-in-diagonal/prime-in-diagonal.java
+++ 2722-prime-in-diagonal/prime-in-diagonal.java
@@ -1,24 +1,24 @@
 class Solution {
-    public static boolean checkPrime(int num){
+    public static boolean checkPrime(int num) {
         int c = 0;
-        for(int j=2;j<=Math.sqrt(num);j++){
-                if(num%j == 0)
-                    return false;
-            }
-            return true;
+        for (int j = 2; j <= Math.sqrt(num); j++) {
+            if (num % j == 0)
+                return false;
+        }
+        return true;
     }
     public int diagonalPrime(int[][] nums) {
-        int k = nums.length-1;
+        int k = nums.length - 1;
         int max = 0;
-        for(int i=0;i<nums.length;i++){
-            if(checkPrime(nums[i][i])){
-                max = Math.max(max,nums[i][i]);
+        for (int i = 0; i < nums.length; i++) {
+            if (checkPrime(nums[i][i])) {
+                max = Math.max(max, nums[i][i]);
             }
             int t = nums[i][k--];
-            if( checkPrime(t)){
-                max = Math.max(max,t);
+            if (checkPrime(t)) {
+                max = Math.max(max, t);
             }
-        }     
-        return max==1 ? 0:max;
+        }
+        return max == 1 ? 0 : max;
     }
 }
