22dff184fbbafafba8538772c72b1cab68515051
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-13 23:23:49 +0530

    Time: 1 ms (99.85%) | Memory: 41.7 MB (85.04%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.java
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -5,16 +5,12 @@ class Solution {
             arr[i] = (int)s.charAt(i);
         }
         int sum = 0;
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
         for(int i=0;i<arr.length-1;i++){
             int t = 0;
             if(arr[i]>arr[i+1])
                 t = arr[i]-arr[i+1];
             else
                 t = arr[i+1]-arr[i];
-                System.out.print(t+" ");
             sum+=t;
         }
         return sum;
