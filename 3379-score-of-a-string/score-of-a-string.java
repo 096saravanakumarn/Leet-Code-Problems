b59153850c3ea9f8591b29a64830f5cc3aed0799
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-13 23:24:09 +0530

    Time: 1 ms (99.85%) | Memory: 41.5 MB (97.59%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.java
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -1,17 +1,17 @@
 class Solution {
     public int scoreOfString(String s) {
-        int arr[] = new int [s.length()];
-        for(int i=0;i<s.length();i++){
-            arr[i] = (int)s.charAt(i);
+        int arr[] = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            arr[i] = (int) s.charAt(i);
         }
         int sum = 0;
-        for(int i=0;i<arr.length-1;i++){
+        for (int i = 0; i < arr.length - 1; i++) {
             int t = 0;
-            if(arr[i]>arr[i+1])
-                t = arr[i]-arr[i+1];
+            if (arr[i] > arr[i + 1])
+                t = arr[i] - arr[i + 1];
             else
-                t = arr[i+1]-arr[i];
-            sum+=t;
+                t = arr[i + 1] - arr[i];
+            sum += t;
         }
         return sum;
     }
