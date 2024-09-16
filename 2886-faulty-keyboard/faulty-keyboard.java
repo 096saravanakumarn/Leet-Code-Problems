245afc9baa04d4314162b941e720ece6418ffd6d
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-16 21:50:26 +0530

    Time: 3 ms (98.22%) | Memory: 44.3 MB (96.74%) - LeetSync

--- 2886-faulty-keyboard/faulty-keyboard.java
+++ 2886-faulty-keyboard/faulty-keyboard.java
@@ -1,14 +1,13 @@
 class Solution {
     public String finalString(String s) {
-          StringBuilder sb = new StringBuilder();
-          for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='i'){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'i') {
                 sb.reverse();
-            }
-            else{
+            } else {
                 sb.append(s.charAt(i));
             }
-          }
-          return sb.toString();
+        }
+        return sb.toString();
     }
 }
