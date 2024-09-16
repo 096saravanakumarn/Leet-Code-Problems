a25f4c9f749102bbddff5dba4bb7cbcf762d9c63
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-16 21:29:35 +0530

    Time: 5 ms (8.12%) | Memory: 41.5 MB (44.90%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.java
+++ 953-reverse-only-letters/reverse-only-letters.java
@@ -1,21 +1,18 @@
 class Solution {
     public String reverseOnlyLetters(String s) {
-        StringBuffer sb = new StringBuffer(s);
         String st = "";
+        char ch[] = s.toCharArray();
         for(int i=s.length()-1;i>=0;i--){
             if(Character.isLetter(s.charAt(i))){
-                st+=sb.charAt(i);
+                st+=ch[i];
             }     
         }
-        System.out.print(sb +" "+st);
-        char ch[] = s.toCharArray();
+        
         int k=0;
         for(int i=0;i<ch.length;i++){
-   
             if(Character.isLetter(ch[i])){
                 ch[i] = st.charAt(k++);
             }  
-   
         }
         return new String(ch);
     }
