d327350e443145385faf67c6a50b8e05b6e0d8d8
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-16 21:30:35 +0530

    Time: 6 ms (7.10%) | Memory: 41.9 MB (9.82%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.java
+++ 953-reverse-only-letters/reverse-only-letters.java
@@ -3,11 +3,10 @@ class Solution {
         String st = "";
         char ch[] = s.toCharArray();
         for(int i=s.length()-1;i>=0;i--){
-            if(Character.isLetter(s.charAt(i))){
+            if(Character.isLetter(ch[i])){
                 st+=ch[i];
             }     
-        }
-        
+        } 
         int k=0;
         for(int i=0;i<ch.length;i++){
             if(Character.isLetter(ch[i])){
