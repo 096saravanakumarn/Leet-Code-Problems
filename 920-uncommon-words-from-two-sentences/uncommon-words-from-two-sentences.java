b505ad4f27f2151d437b56b359101a8445056519
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-17 15:52:18 +0530

    Time: 2 ms (100.00%) | Memory: 41.9 MB (69.28%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.java
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.java
@@ -1,21 +1,20 @@
 class Solution {
     public String[] uncommonFromSentences(String s1, String s2) {
-        Map<String,Integer> map = new HashMap<>();
-        for(String s:s1.split(" ")){
-            map.put(s,map.getOrDefault(s,0)+1);
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : s1.split(" ")) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
         }
-        for(String s:s2.split(" ")){
-            map.put(s,map.getOrDefault(s,0)+1);
+        for (String s : s2.split(" ")) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
         }
         List<String> list = new ArrayList<>();
-        for(Map.Entry<String,Integer> e : map.entrySet()){
-            if(e.getValue() == 1){
+        for (Map.Entry<String, Integer> e : map.entrySet()) {
+            if (e.getValue() == 1) {
                 list.add(e.getKey());
             }
         }
-        
         String arr[] = new String[list.size()];
-        for(int i=0;i<arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = list.get(i);
         }
         return arr;
