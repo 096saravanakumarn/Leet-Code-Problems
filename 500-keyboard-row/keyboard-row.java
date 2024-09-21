99d0815f5c522ac671b7e56e5a4db137c64216ba
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-21 14:24:11 +0530

    Time: 1 ms (54.55%) | Memory: 41.5 MB (26.93%) - LeetSync

--- 500-keyboard-row/keyboard-row.java
+++ 500-keyboard-row/keyboard-row.java
@@ -4,32 +4,36 @@ class Solution {
         String s2 = "asdfghjklASDFGHJKL";
         String s3 = "zxcvbnmZXCVBNM";
         List<String> list = new ArrayList<>();
-        for(int i=0;i<words.length;i++){
+        for (int i = 0; i < words.length; i++) {
             int c = 0;
             int count = 0;
-            if(s1.contains(words[i].charAt(0)+"")) c = 1;
-            if(s2.contains(words[i].charAt(0)+"")) c = 2;
-            if(s3.contains(words[i].charAt(0)+"")) c = 3;
-            if(c == 1){
-                for(int j=0;j<words[i].length();j++){
-                    if(s1.contains(words[i].charAt(j)+"")) count ++;
+            if (s1.contains(words[i].charAt(0) + ""))
+                c = 1;
+            if (s2.contains(words[i].charAt(0) + ""))
+                c = 2;
+            if (s3.contains(words[i].charAt(0) + ""))
+                c = 3;
+            if (c == 1) {
+                for (int j = 0; j < words[i].length(); j++) {
+                    if (s1.contains(words[i].charAt(j) + ""))
+                        count++;
+                }
+            } else if (c == 2) {
+                for (int j = 0; j < words[i].length(); j++) {
+                    if (s2.contains(words[i].charAt(j) + ""))
+                        count++;
+                }
+            } else if (c == 3) {
+                for (int j = 0; j < words[i].length(); j++) {
+                    if (s3.contains(words[i].charAt(j) + ""))
+                        count++;
                 }
             }
-            else if(c == 2){
-                for(int j=0;j<words[i].length();j++){
-                    if(s2.contains(words[i].charAt(j)+"")) count ++;
-                }
-            }
-            else if(c == 3){
-                for(int j=0;j<words[i].length();j++){
-                    if(s3.contains(words[i].charAt(j)+"")) count ++;
-                }
-            }
-            if(count == words[i].length()) 
-                   list.add(words[i]);    
+            if (count == words[i].length())
+                list.add(words[i]);
         }
         String result[] = new String[list.size()];
-        for(int i=0;i<list.size();i++){
+        for (int i = 0; i < list.size(); i++) {
             result[i] = list.get(i).trim();
         }
         return result;
