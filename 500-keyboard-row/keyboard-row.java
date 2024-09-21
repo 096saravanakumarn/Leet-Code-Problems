23e2574ee800b454653573a88b6b30a060687f5b
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-21 14:24:53 +0530

    Time: 2 ms (19.43%) | Memory: 40.9 MB (97.23%) - LeetSync

--- 500-keyboard-row/keyboard-row.java
+++ 500-keyboard-row/keyboard-row.java
@@ -34,7 +34,7 @@ class Solution {
         }
         String result[] = new String[list.size()];
         for (int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i).trim();
+            result[i] = list.get(i);
         }
         return result;
     }
