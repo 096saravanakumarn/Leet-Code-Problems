c267e842ae1c1f2c0928ed47be8c22f40a0c7841
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-21 14:19:57 +0530

    Time: 1 ms (54.55%) | Memory: 41.2 MB (75.39%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String[] findWords(String[] words) {
+        String s1 = "qwertyuiopQWERTYUIOP";
+        String s2 = "asdfghjklASDFGHJKL";
+        String s3 = "zxcvbnmZXCVBNM";
+        List<String> list = new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            int c = 0;
+            if(s1.contains(words[i].charAt(0)+"")) c = 1;
+            if(s2.contains(words[i].charAt(0)+"")) c = 2;
+            if(s3.contains(words[i].charAt(0)+"")) c = 3;
+            int count = 0;
+            if(c == 1){
+                for(int j=0;j<words[i].length();j++){
+                    if(s1.contains(words[i].charAt(j)+"")) count ++;
+                }
+            }
+            else if(c == 2){
+                for(int j=0;j<words[i].length();j++){
+                    if(s2.contains(words[i].charAt(j)+"")) count ++;
+                }
+            }
+            else if(c == 3){
+                for(int j=0;j<words[i].length();j++){
+                    if(s3.contains(words[i].charAt(j)+"")) count ++;
+                }
+            }
+    
+            if(count == words[i].length()) 
+            if(i != words.length-1)
+                   list.add(words[i]+" ");
+            else list.add(words[i]);
+
+        }
+        String result[] = new String[list.size()];
+        for(int i=0;i<list.size();i++){
+            result[i] = list.get(i).trim();
+        }
+        return result;
+    }
+}
