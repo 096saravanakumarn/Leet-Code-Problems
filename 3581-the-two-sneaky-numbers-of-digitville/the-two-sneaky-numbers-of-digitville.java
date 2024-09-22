b258be3b4bc5112719570ccd832311e6305268e3
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-22 15:15:01 +0530

    Time: 3 ms (40.79%) | Memory: 45.2 MB (8.98%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -4,17 +4,17 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        int c = 0;
+        
         int i = 0;
         int arr[] = new int[2];
         for(Map.Entry<Integer,Integer> e:map.entrySet()){
             if(e.getValue() == 2){
                 arr[i++] = e.getKey();
-                c++;
+             
             }
         }
-        if(c == 2)
+      
             return arr;
-        return new int[]{};
+    
     }
 }
