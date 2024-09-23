4109f239603507c78ae01e4668fadef626667a1d
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-23 09:17:31 +0530

    Time: 1 ms (32.41%) | Memory: 42.2 MB (14.54%) - LeetSync

--- 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.java
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.java
@@ -3,10 +3,11 @@ class Solution {
         int arr[][] = { { 0, 1, 0, 1, 0, 1, 0, 1 }, { 1, 0, 1, 0, 1, 0, 1, 0 }, { 0, 1, 0, 1, 0, 1, 0, 1 },
                 { 1, 0, 1, 0, 1, 0, 1, 0 }, { 0, 1, 0, 1, 0, 1, 0, 1 }, { 1, 0, 1, 0, 1, 0, 1, 0 },
                 { 0, 1, 0, 1, 0, 1, 0, 1 }, { 1, 0, 1, 0, 1, 0, 1, 0 } };
-        if (arr[Integer.parseInt(coordinate1.substring(1, coordinate1.length())) - 1][coordinate1
-                .substring(0, coordinate1.length() - 1).charAt(0)
-                - 'a'] == arr[Integer.parseInt(coordinate2.substring(1, coordinate2.length())) - 1][coordinate2
-                        .substring(0, coordinate2.length() - 1).charAt(0) - 'a']) {
+        String s1 = coordinate1.substring(0, coordinate1.length() - 1);
+        String s2 = coordinate2.substring(0, coordinate2.length() - 1);
+        String s4 = coordinate1.substring(1, coordinate1.length());
+        String s5 = coordinate2.substring(1, coordinate2.length());
+        if (arr[Integer.parseInt(s4) - 1][s1.charAt(0) - 'a'] == arr[Integer.parseInt(s5) - 1][s2.charAt(0) - 'a']) {
             return true;
         }
         return false;
