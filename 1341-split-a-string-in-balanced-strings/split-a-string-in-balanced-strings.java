471228a2932e9547095a7843afdc02ed0d9a3a01
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-09-24 12:34:14 +0530

    Time: 1 ms (20.22%) | Memory: 41.6 MB (7.40%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int rc = 0,lc = 0;
+        int count = 0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == 'R') rc++;
+            else if(s.charAt(i) == 'L') lc++;
+            if(rc == lc) {
+                count++;
+                rc = 0;
+                lc = 0;
+            }
+        }
+        return count;
+    }
+}
