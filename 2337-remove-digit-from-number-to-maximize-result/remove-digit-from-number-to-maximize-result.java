4b9e217234343f4ac770c9a28a406b219e7d6fcc
SARAVANAKUMAR N <137426682+096saravanakumarn@users.noreply.github.com>
2024-10-01 00:10:21 +0530

    Time: 10 ms (6.58%) | Memory: 43.9 MB (12.22%) - LeetSync

--- 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.java
+++ 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.java
@@ -1,10 +1,11 @@
 import java.math.BigInteger;
+
 class Solution {
     public String removeDigit(String number, char digit) {
         List<BigInteger> list = new ArrayList<>();
-        for(int i=0;i<number.length();i++){
+        for (int i = 0; i < number.length(); i++) {
             StringBuilder sb = new StringBuilder(number);
-            if(number.charAt(i) == digit){
+            if (number.charAt(i) == digit) {
                 sb.deleteCharAt(i);
                 list.add(new BigInteger(sb.toString()));
             }
